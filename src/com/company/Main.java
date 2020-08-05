545528e970bee2bd195b30d0dc6739fd03574c97
Aijanss <68231963+Aijanss@users.noreply.github.com>
2020-08-06 00:06:58 +0600

    switch

---
+++ src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        String name = "Ramazan";
+        switch (name) {
+            case "Marlen":
+                System.out.println("Hi Marlen");
+            case "Marat":
+                System.out.println("Hi Marat");
+            case "Ramazan":
+                System.out.println("Hi Ramazan");
+        }
+        int number = 20;
+        switch (number) {
+            case 1:
+                System.out.println("Число равно дисяти");
+            case 2:
+                System.out.println("Число равно пяти ");
+            case 3:
+                System.out.println("Число равно двацати ");
+            default:
+                System.out.println("Число не известно");
+
+
+        }
+
+    }
+
+
+}
+
